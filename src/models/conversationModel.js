2cb0eac05f9ac6184bec89b9ef60e8a561c00556
AjijSeikh <ajijsk240@gmail.com>
2023-09-30 10:09:05 +0530

    search user

--- src/models/conversationModel.js
+++ src/models/conversationModel.js
@@ -6,6 +6,10 @@ const conversationSchema= new mongoose.Schema({
         required:[true, "Conversation name is required"],
         trim: true
     },
+    picture:{
+    type: String,
+    required:true,
+    },
     isGroup:{
         type:Boolean,
         required:true,
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./auth.route.js";
+import userRoutes from "./user.route.js"
 import conversationRoutes from "./conversation.route.js"
 import messageRoutes from "./message.route.js"
 
@@ -7,6 +8,8 @@ const router = express.Router();
 
 // this routes is global route
 router.use("/auth",authRoutes)
+router.use("/user",userRoutes)
+
 router.use("/conversation",conversationRoutes)
 router.use("/message",messageRoutes)
 
