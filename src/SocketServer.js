930989033bfe32e165bcc7a35dfa16963bd2fb7a
AjijSeikh <ajijsk240@gmail.com>
2023-10-06 11:24:43 +0530

    realtime socket io completed

--- src/SocketServer.js
+++ src/SocketServer.js
@@ -1,8 +1,23 @@
-export default function (socket) {
+let onlineUsers = [];
+export default function (socket, io) {
   /// user joins or opens the application
   socket.on("join", (user) => {
     socket.join(user);
+    // add joined user to online users
+    if (!onlineUsers.some((u) => u.userId === user)) {
+      console.log(`user ${user} is now online`);
+      onlineUsers.push({ userId: user, socketId: socket.id });
+    }
+    // send online users to frontend
+    io.emit("get-online-users", onlineUsers);
   });
+  // socket disconnect
+  socket.on("disconnect", () => {
+    onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+
+    socket.emit("get-online-users", onlineUsers);
+  });
+
   // join a conversation room
   socket.on("join conversation", (conversation) => {
     socket.join(conversation);
@@ -17,4 +32,15 @@ export default function (socket) {
       socket.in(user._id).emit("receive message", message);
     });
   });
+
+  // Typing 
+  socket.on("typing",(conversation)=>{
+    console.log("typing msg...",conversation);
+    socket.in(conversation).emit("typing",conversation);
+  })
+  socket.on("stop typing",(conversation)=>{
+    console.log(" stop typing msg...",conversation);
+    socket.in(conversation).emit("stop typing");
+  })
 }
+
